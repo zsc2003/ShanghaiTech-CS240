a32251d87c3c3efec3c82a45441589d8ea9374f8
zhoushch <zhoushch@shanghaitech.edu.cn>
2024-06-03 20:01:25 +0800

    finished t1

--- project/t1.cpp
+++ project/t1.cpp
@@ -10,13 +10,52 @@ inline int read()
 
 int main()
 {
+    const char* patten = "wbwbwwbwbwbwwbwbwwbwbwbw";
+    int len = 24;
     int n = read();
     while (n--)
     {
-        int w = read(), b = read();
+        int w_input = read(), b_input = read();
 
-        std::cout << "Yes ";
-        std::cout << "No ";
+        // patten: wbwbwwbwbwbw 12, w: 7, b: 5
+        int cycle_w = w_input / 7;
+        int cycle_b = b_input / 5;
+        int cycle = std::min(cycle_w, cycle_b);
+
+        w_input -= cycle * 7;
+        b_input -= cycle * 5;
+
+        bool flag = 0;
+        for (int i = 0; i < len; ++i)
+        {
+            if (flag)
+                break;
+            int w = w_input, b = b_input;
+            for (int j = i; j < len; ++j)
+            {
+                if (patten[j] == 'w')
+                {
+                    --w;
+                    if (w < 0)
+                        break;
+                }
+                else
+                {
+                    --b;
+                    if (b < 0)
+                        break;
+                }
+                if (w == 0 && b == 0)
+                {
+                    flag = 1;
+                    std::cout << "Yes ";
+                    break;
+                }
+            }
+        }
+
+        if (!flag)
+            std::cout << "No ";
     }
 
     return 0;
