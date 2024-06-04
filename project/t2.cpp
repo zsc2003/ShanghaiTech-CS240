6ac3eae41b45fa8c6f3ce9377cba7fcbd44d4d39
zhoushch <zhoushch@shanghaitech.edu.cn>
2024-06-04 22:09:09 +0800

    finished t2

--- project/t2.cpp
+++ project/t2.cpp
@@ -9,15 +9,38 @@ inline int read()
     return s ^ 45 ? r : -r;
 }
 
+const int N = 200010;
+const int M = 10;
+bool dp[N][M]; // dp[i][j]: whether s[1: i] could be generated, pos[i] is the last position of t[1: j]
+
 int main()
 {
     int n = read(), m = read();
     std::string s, t;
     std::cin >> s >> t;
 
+    dp[0][0] = (s[0] == t[0]);
+    for (int i = 1; i < n; ++i)
+    {
+        if (s[i] == t[0]) // right covers left
+        {
+            for (int j = 0; j < m; ++j)
+                dp[i][0] |= dp[i - 1][j];
+        }
 
-    puts("Yes");
-    puts("No");
+        for (int j = 0; j < m; ++j)
+        {
+            if (s[i] == t[j])
+            {
+                if (j != 0) // continue the previous coverage
+                    dp[i][j] |= dp[i - 1][j - 1];
+
+                dp[i][j] |= dp[i - 1][m - 1]; // left covers right
+            }
+        }
+    }
+
+    puts(dp[n - 1][m - 1] ? "Yes" : "No");
 
     return 0;
 }
