3edcf3fe77e81a64668a42ada7825e83760534b4
zhoushch <zhoushch@shanghaitech.edu.cn>
2024-06-17 19:11:07 +0800

    finished t4

--- project/t4.cpp
+++ project/t4.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <algorithm>
+#include <cmath>
+#include <queue>
 
 inline int read()
 {
@@ -15,21 +17,46 @@ struct node
     int l, r;
 }a[N];
 
+int ans[N];
+
 int main()
 {
     int n = read();
+    int lmin = N << 1, rmax = 0;
     for (int i = 1; i <= n; ++i)
-        a[i].l = read(), a[i].r = read();
-
-    std::sort(a + 1, a + n + 1, [](const node &x, const node &y) { return x.r < y.r; });
-
-
-    for (int k = 1; k <= n; ++k)
     {
-
-
-
+        a[i].l = read(), a[i].r = read();
+        lmin = std::min(lmin, a[i].l);
+        rmax = std::max(rmax, a[i].r);
     }
+
+    std::sort(a + 1, a + 1 + n, [](const node &a, const node &b) { return a.l < b.l; });
+
+    for (int i = lmin; i <= rmax; ++i) // conference start time
+    {
+        int k = 1; // lecturer
+        std::priority_queue<int, std::vector<int>, std::greater<int>> q{}; // min heap
+
+        for (int j = i; (j <= rmax) && (j - i + 1 <= n); ++j) // conference end time
+        {
+            while(k <= n && a[k].l <= j)
+                q.push(a[k++].r);
+
+            while(!q.empty() && q.top() < j)
+                q.pop();
+
+            if (q.empty())
+                break;
+
+            // std::cout << "Current q.top() = " << q.top() << std::endl;
+            // std::cout << i << ' ' << j << std::endl;
+            ++ans[j - i + 1];
+            q.pop();
+        }
+    }
+
+    for (int i = 1; i <= n; ++i)
+        printf("%d\n", ans[i]);
 
     return 0;
 }
